e898bcf9abdeba031a7929ed0c479605a606d699
Nikita Romashov <65943108+NikitaRomashov@users.noreply.github.com>
2020-05-29 00:32:37 +1000

    day_1

---
+++ js/script.js
@@ -0,0 +1,69 @@
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});;
+$(document).ready(function () {
+	$('.tab__link').click(function (event) {
+		$('.tab__link,.tab__element').toggleClass('active');
+
+	});
+});;
+
+
+
+$('.multiple-items').slick({
+	infinite: true,
+	slidesToShow: 2,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	variableWidth: true,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 564,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
+
+});
